e33ecbd236d4bbcd2f34dc884ad5d9f3149ac035
NISHE-SHOHA <nazmakhatunnishe@gmail.com>
2022-01-07 23:36:02 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Home from './components/Home/Home';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
